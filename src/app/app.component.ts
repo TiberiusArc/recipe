61f54ae675428f4dc250cc05e740b3703345a622
rossn <ross.norman@comtrade.com>
2018-06-07 14:03:04 +0100

    Merged local repo with remote

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  loadedFeature = 'recipe';
+
+  ngOnInit() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyAUetQU-1HHOdeg9l1KTYYQAh1j-2ltLog",
+      authDomain: "ng-recipe-33578.firebaseapp.com"
+    });
+  }
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
